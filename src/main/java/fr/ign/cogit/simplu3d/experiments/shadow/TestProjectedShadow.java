8319097ccee8a2cee451e738e7576407cd52757d
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-03-31 21:46:41 +0200

    Standalone execution of ProjectedShadow

--- src/main/java/fr/ign/cogit/simplu3d/experiments/shadow/TestProjectedShadow.java
+++ src/main/java/fr/ign/cogit/simplu3d/experiments/shadow/TestProjectedShadow.java
@@ -14,7 +14,6 @@ import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
 import fr.ign.cogit.geoxygene.contrib.geometrie.Vecteur;
 import fr.ign.cogit.geoxygene.feature.DefaultFeature;
 import fr.ign.cogit.geoxygene.feature.FT_FeatureCollection;
-import fr.ign.cogit.geoxygene.sig3d.calculation.raycasting.InverseProjection;
 import fr.ign.cogit.geoxygene.sig3d.calculation.raycasting.ProjectedShadow;
 import fr.ign.cogit.geoxygene.sig3d.calculation.raycasting.RayCasting;
 import fr.ign.cogit.geoxygene.sig3d.equation.PlanEquation;
@@ -43,21 +42,21 @@ import fr.ign.cogit.simplu3d.representation.RepEnvironnement.Theme;
 public class TestProjectedShadow {
 
 	public static void main(String[] args) throws Exception {
-		//Epsilon to check if a ray intersects a polygon
+		// Epsilon to check if a ray intersects a polygon
 		RayCasting.EPSILON = 0.01;
-		//Add a special test to check if the ray intersects the edge of a geometry
+		// Add a special test to check if the ray intersects the edge of a geometry
 		RayCasting.CHECK_IS_ON_EDGE = true;
-		
+
 		PlanEquation.EPSILON = 0.0000000000001;
-		
-		//Sky color
+
+		// Sky color
 		ConstantRepresentation.backGroundColor = new Color(156, 180, 193);
-		
-		//SimPLU3D project
-		String folder = "E:/mbrasebin/Donnees/Strasbourg/GTRU/Project1/";
+
+		// SimPLU3D project
+		String folder = TestProjectedShadow.class.getClassLoader().getResource("data/projectShadow/").getPath();
 
 		Environnement env = LoaderSHP.load(new File(folder));
-		//Rendered themes
+		// Rendered themes
 		List<Theme> lTheme = new ArrayList<RepEnvironnement.Theme>();
 		lTheme.add(Theme.TOIT_BATIMENT);
 		lTheme.add(Theme.FACADE_BATIMENT);
@@ -71,7 +70,7 @@ public class TestProjectedShadow {
 		// lTheme.add(Theme.PAN);
 
 		Theme[] tab = lTheme.toArray(new Theme[0]);
-		//Prepare the rendering
+		// Prepare the rendering
 		List<VectorLayer> vl = RepEnvironnement.represent(env, tab);
 
 		MainWindow mW = new MainWindow();
@@ -80,19 +79,13 @@ public class TestProjectedShadow {
 
 			mW.getInterfaceMap3D().getCurrent3DMap().addLayer(l);
 		}
-		//rEMOVE THE LIGHT
+		// rEMOVE THE LIGHT
 		mW.getInterfaceMap3D().removeLight(0);
 
-		/*
-		 * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
-		 * mW.getInterfaceMap3D().moveLight(180, -15, 120,0);
-		 * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
-		 * mW.getInterfaceMap3D().moveLight( -140, 3, 120,1);
-		 */
-
+		// This part is only used to texture the ground to make a prette scene <3
 		// 1051042.8513268954120576,6840539.0837931865826249 :
 		// 1051264.8064121364150196,6840679.2711814027279615
-		//sCENE COORDINATES
+		// SCENE COORDINATES
 		double xc = (1051042.8513268954120576 + 1051264.8064121364150196) / 2;
 		double yc = (6840539.0837931865826249 + 6840679.2711814027279615) / 2;
 
@@ -113,7 +106,7 @@ public class TestProjectedShadow {
 		dpl.add(dp3);
 		dpl.add(dp4);
 		dpl.add(dp1);
-		//Emprise to map an image
+		// Emprise to map an image
 		IPolygon emprise = new GM_Polygon(new GM_LineString(dpl));
 
 		if (emprise.isEmpty()) {
@@ -138,41 +131,26 @@ public class TestProjectedShadow {
 
 		long t = System.currentTimeMillis();
 
-		//Processing the shadow casting with
-		//--SimPLU3D Buildings
-		//--Parcels (not sure if they are useful)
-		//--Scene geometry (on which shadow will be casted)
-		//--Sun direction vector
-		//--Min and max distance of projection
-		//--Type of result (here volumic shadows)
-		//--Do not remember what is true
+		// Processing the shadow casting with
+		// --SimPLU3D Buildings
+		// --Parcels : shadow is projected on parcels
+		// --If parcel is empty or there is not intersection, shadow is projected on
+		// scene footprint
+		// --Sun direction vector
+		// --Min and max distance of projection
+		// --Type of result (here volumic shadows)
+		// --Do not remember what is true
 		List<IGeometry> lGeom2 = ProjectedShadow.process(env.getBuildings(), env.getSubParcels(),
 				(IPolygon) feat.getGeom(), new Vecteur(0, 1, -0.4), 0, 15,
 				ProjectedShadow.POSSIBLE_RESULT.PROJECTED_VOLUME, true);
 
-		List<IGeometry> lGeom = InverseProjection.process(env.getBuildings(), 4, new Vecteur(0, 1, -0.4), 20);
-
 		System.out.println("Temps écoulé " + (System.currentTimeMillis() - t));
 
-		IFeatureCollection<IFeature> lfeatC = new FT_FeatureCollection<IFeature>();
 		IFeatureCollection<IFeature> lfeatC2 = new FT_FeatureCollection<IFeature>();
-
-		// List<IOrientableCurve> lC =
-		// CalculNormales.getNormal(env.getBatiments(),
-		// 5);
-		// lGeom.addAll(lC);
 
 		for (IGeometry geom : lGeom2) {
 			lfeatC2.add(new DefaultFeature(geom));
 		}
-
-		for (IGeometry geom : lGeom) {
-			lfeatC.add(new DefaultFeature(geom));
-		}
-
-		System.out.println("NB Point : " + lfeatC.size());
-
-		mW.getInterfaceMap3D().getCurrent3DMap().addLayer(new VectorLayer(lfeatC, "Points", Color.pink));
 
 		mW.getInterfaceMap3D().getCurrent3DMap().addLayer(new VectorLayer(lfeatC2, "Points2", Color.pink));
 
